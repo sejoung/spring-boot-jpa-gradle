639b75aca7baab0e85be62b75ed5a39ca52d4fa7
sejoung kim <sejoung@gmail.com>
2021-06-23 09:12:57 +0900

    ㅁㄴㅇ

--- src/main/java/io/github/sejoung/jpa/domain/PackageProduct.java
+++ src/main/java/io/github/sejoung/jpa/domain/PackageProduct.java
@@ -3,10 +3,6 @@ package io.github.sejoung.jpa.domain;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.PrimaryKeyJoinColumn;
 
 import io.github.sejoung.jpa.constants.ProductType;
 import lombok.AccessLevel;
